6598354603a1989864a76789282b4485364b10b4
BlackPigCompany <86426768+BlackPigCompany@users.noreply.github.com>
2024-10-10 14:52:21 +0200

    table todos  + insert value

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -12,6 +13,11 @@ class UserController extends Controller
      */
     public function index()
     {
+        $todo = new Todo;
+        $todo->name = 'todo 1';
+        $todo->user_id = '1';
+        $todo->save();
+
         $users = User::all();
         return view('users.index', compact('users'));
     }
--- database/migrations/2024_10_10_115604_create_todos_table.php
+++ database/migrations/2024_10_10_115604_create_todos_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->boolean('is_check')->default(false);
             $table->timestamps();
+            $table->foreignId("user_id")->constrained()->onDelete('cascade');
         });
     }
 
